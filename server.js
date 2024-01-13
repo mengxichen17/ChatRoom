6bc0c414d8f2f4393182e85cdecd42688bac56ea
mengxichen17 <yjjslsyq@gmail.com>
2024-01-13 11:53:13 -0800

    followed smart-brain and working, need to change to chatroom

--- server.js
+++ server.js
@@ -1,57 +1,91 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : '5432',
+      user : 'mengxi',
+      password : '',
+      database : 'chat-history'
+    }
+  });
 
 const app = express();
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        },
-    ]
-}
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success');
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json('success');
-    } else {
-        res.status(400).json('error logging in');
-    }
+    db.select('name', 'hash').from('login')
+        .where('name', '=', req.body.name)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                        .where('name', '=', req.body.name)
+                        .then(user => {
+                            res.json(user[0])
+                        })
+                        .catch(error => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { name, password } = req.body;
-    database.users.push(
-        {
-            id: '125',
-            name: name,
-            password: password,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-    res.json(database.users[database.users.length - 1])
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                name: name
+            })
+            .into('login')
+            .returning('name')
+            .then(loginName => {
+                console.log(loginName);
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: loginName[0].name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback) // if any wrong registration, roll back
+        })
+        
+        .catch(err => res.status(400).json('unable to register'));
 })
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+      .then(user => {
+        if (user.length) {
+          res.json(user[0]);
+        } else {
+          res.status(400).json('Not found');
+        }
+      })
+      .catch(err => res.status(400).json('error getting user'));
+  })
+
+app.get('/chathistory/ ')
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
