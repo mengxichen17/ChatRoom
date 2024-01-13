997abe74013692ae34be0c428c9c6577c13d2c53
mengxichen17 <yjjslsyq@gmail.com>
2024-01-12 18:22:12 -0800

    initial back-end

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
+app.use(bodyParser.json());
+app.use(cors());
+// app.use(express.bodyParser());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { name, password } = req.body;
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    )
+    res.json(database.users[database.users.length - 1])
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/chathistory --> GET = chathistory
+
+*/
